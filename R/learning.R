bf5f066bd35cdf1334fb82cb51e1370a2382bfcf
Andrea Ramnath <andrea.ramnath@gmail.com>
2023-01-16 16:04:13 +0100

    Add initial R/learning.R file

---
+++ R/learning.R
@@ -0,0 +1,27 @@
+10
+# cntrl shft p for new section
+# R basics ----------------------------------------------------------------
+# shrtcut for <- is optn -
+weight_kilos <- 100
+weight_kilos <- 10
+
+weight_kilos
+#autocompletion tools
+# cmd entr to send to consol
+colnames(airquality)
+
+str(airquality)
+
+summary(airquality)
+
+#making code more readable
+# code reformat code or cntrl shft a will restyle
+2 + 2
+#cmd shft p then stle activel file
+2 + 2
+
+
+# Packages ----------------------------------------------------------------
+#when loading opackage then need to call library so have access to all
+#put at start of script
+library(tidyverse)
