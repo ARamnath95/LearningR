e2e4b0e9caa485e1894e275c44c7de21ce675348
Andrea Ramnath <andrea.ramnath@gmail.com>
2023-01-17 13:40:04 +0100

    Arranging

--- R/learning.R
+++ R/learning.R
@@ -103,15 +103,40 @@ nhanes_small %>%
 # Filtering rows ----------------------------------------------------------
 
 nhanes_small %>%
-    filter(phys_active!="No")
+  filter(phys_active != "No")
 
 nhanes_small %>%
-    filter(bmi>=25,
-           phys_active=="No")
+  filter(
+    bmi >= 25,
+    phys_active == "No"
+  )
 
 nhanes_small %>%
-    filter(bmi==25|
-               phys_active=='No')
+  filter(bmi == 25 |
+    phys_active == "No")
+
+
+# Arranging rows ----------------------------------------------------------
+
+nhanes_small %>%
+  arrange(desc(age), bmi, education)
+
+
+# Mutating columns --------------------------------------------------------
+#works consequentially, so weeks can be calculated after months have been calculated.
+nhanes_update <- nhanes_small %>%
+  mutate(
+    age_months = age * 12,
+    logged_bmi=log(bmi),
+    age_weeks=age_months*4,
+    old=if_else(
+        age>=30,
+        "old",
+        "young"
+    )
+  )
+
+nhanes_update
 
 
 
